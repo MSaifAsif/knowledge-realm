2ebe8cbe17bc7935313f089683b4f09b43079e73
Saif Mirza <saif.mirza@de.bosch.com>
2020-01-17 16:57:45 +0100

    idea

--- athenaeum/views.py
+++ athenaeum/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets, response
+from .models import Resource
+from .serializers import ResourceSerializer
 
-# Create your views here.
+
+class ResourceViewSet(viewsets.ModelViewSet):
+    queryset = Resource.objects.all()
+    lookup_field = 'id'
+    serializer_class = ResourceSerializer
